fa0e3c05c764ab4ce0d2f3acf8e3c3e67eafab80
siham el asli <152698009+SihamElasli@users.noreply.github.com>
2026-04-24 00:56:15 +0100

    fix: reset alembic migrations and clean schema

--- alembic/env.py
+++ alembic/env.py
@@ -4,11 +4,14 @@ from alembic import context
 
 from src.config.config import settings
 from src.config.db import Base
-from src.auth.models.user import User  # noqa: F401
-from src.project.models.project import Project  # noqa: F401
-from src.environment.models.Environment import Environment  # noqa: F401
-from src.environment.models.Task_type import TaskType  # noqa: F401
-from src.environment.models.Environment_status import EnvironmentStatus  # noqa: F401
+from src.auth.models.user import User
+
+from src.project.models.project import Project
+
+from src.environment.models.Environment import Environment
+from src.environment.models.Task_type import TaskType
+from src.environment.models.Environment_status import EnvironmentStatus
+
 from src.runs.models.run import Run, TrainingConfig  # noqa: F401
 
 
