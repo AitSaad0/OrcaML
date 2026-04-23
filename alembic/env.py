96a8a665cca0201ab52d7f2b5f25df8b26f5ae97
mouadbakhta <mouadbakhta939@gmai.com>
2026-04-23 17:29:08 +0000

    fix : ci alembic

--- alembic/env.py
+++ alembic/env.py
@@ -9,6 +9,7 @@ from src.project.models.project import Project  # noqa: F401
 from src.environment.models.Environment import Environment  # noqa: F401
 from src.environment.models.Task_type import TaskType  # noqa: F401
 from src.environment.models.Environment_status import EnvironmentStatus  # noqa: F401
+from src.dataset.models.dataset import Dataset # noqa: F401
 
 
 config = context.config
