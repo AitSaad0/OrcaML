f659423291ee39bb448caac243125ea33afc5d95
mouadbakhta <mouadbakhta939@gmai.com>
2026-05-06 21:09:35 +0000

    fix : env.py

--- alembic/env.py
+++ alembic/env.py
@@ -4,16 +4,23 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-# On force Python à trouver ton dossier 'src'
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.config.db import Base
 from src.config.config import settings
 
 # --- IMPORT OBLIGATOIRE ---
-# Si tu n'importes pas les classes ici, Alembic ne créera RIEN
+# Ces imports sont nécessaires pour qu'Alembic détecte les tables
+# noqa comments prevent ruff from removing them
+from src.auth.models.user import User  # noqa: F401
+from src.project.models.project import Project  # noqa: F401
+from src.environment.models.Environment import Environment  # noqa: F401
+from src.dataset.models.dataset import Dataset  # noqa: F401
+from src.dataset.models.cleaning_config import CleaningConfig  # noqa: F401
+from src.dataset.models.cleaned_dataset import CleanedDataset  # noqa: F401
 
 config = context.config
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
