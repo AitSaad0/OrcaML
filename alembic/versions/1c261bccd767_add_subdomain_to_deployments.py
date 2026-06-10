c949f7431572cfe72327ab8ba537600483325acf
AitSaad0 <saadaitabd2004@email.com>
2026-06-10 21:04:15 +0100

    fix(migration): use IF NOT EXISTS for subdomain column

--- alembic/versions/1c261bccd767_add_subdomain_to_deployments.py
+++ alembic/versions/1c261bccd767_add_subdomain_to_deployments.py
@@ -8,7 +8,6 @@ Create Date: 2026-06-10 17:51:56.886109
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '1c261bccd767'
